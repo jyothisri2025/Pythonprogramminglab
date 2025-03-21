b9c3d9542f7b5795ac65abee766508289ab2ff7e
jyothisri2025 <24ag1a67b1@gmail.com>
2025-03-21 15:48:28 +0530

    Add files via upload

---
+++ week1/dist.py
@@ -0,0 +1,10 @@
+import math
+x1=int(input("enter the 1st number:"))
+x2=int(input("enter the 2nd number:"))
+y1=int(input("enter the 3rd number:"))
+y2=int(input("enter the 4th number:"))
+x=(x2-x1)
+z=(y2-y1)
+dist=math.sqrt((x**2)+(z**2))
+print(dist)
+ 
